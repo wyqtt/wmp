abd00ce0f749ff356100baefb3e4d9e8d9a037d9
wyqtt <wyattmimportant@gmail.com>
2026-09-02 08:14:00 -0300

    fix: app finder

--- patches/src/main/kotlin/app/template/patches/appfinder/AppFinderPatch.kt
+++ patches/src/main/kotlin/app/template/patches/appfinder/AppFinderPatch.kt
@@ -1,8 +1,10 @@
 package wy.morphe.patches.appfinder
 
+import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.morphe.patcher.patch.bytecodePatch
 import wy.morphe.patches.shared.Constants.APP_FINDER_COMPATIBILITY
 
+@Suppress("unused")
 val appFinderPatch = bytecodePatch(
     name = "Pro",
     description = "Enable pro.",
@@ -11,15 +13,12 @@ val appFinderPatch = bytecodePatch(
 
     execute {
         // Target the getB() method which returns subscription status
-        PremiumCheckFingerprint.method.apply {
-            // Replace method body to always return true
-            addInstructions(
-                0,
-                """
-                    const/4 v0, 0x1
-                    return v0
-                """
-            )
-        }
+        PremiumCheckFingerprint.method.addInstructions(
+            0,
+            """
+                const/4 v0, 0x1
+                return v0
+            """.trimIndent(),
+        )
     }
 }
